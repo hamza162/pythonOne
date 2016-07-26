63ed7b6897d6f1bf68d4c26a4b920ce626620d96
Hamza Zaman <hamza162@gmail.com>
2016-07-26 19:20:26 +0000

    second

--- loops.py
+++ loops.py
@@ -1,4 +1,14 @@
-ar = [9,8,3,2,4,5,6,4]
+    
+def wutfunc(array):
+    for x in array:
+        if x%2 == 0 or x ==1:
+            print "yeah evens"
+        else:
+            print "yeah odds"
 
-for x in ar:
-        print x
+ar = [1,8,3,2,4,5,6,4]
+ab = [5,3,5,56,7,43,7]
+hah = ar + ab
+print hah
+wutfunc(hah)
+
